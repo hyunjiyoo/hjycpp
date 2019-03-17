ac9c9a23cb8ddb8d254545fa9b415222c094c46d
hyunjiyoo <solteee23@gmail.com>
2019-03-17 20:07:02 +0900

    first

---
+++ codes/ch03/Pwd/Pwd/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+	char pwd[100];
+	while (true) 
+	{
+		cout << "¾ÏÈ£ ÀÔ·Â: ";
+		cin.getline(pwd, 100, '\n');
+		if (strcmp(pwd, "alpha go") == 0) {
+			cout << "Correct password" << endl;
+			break; // loop Å»Ãâ
+		} else if (strcmp(pwd, "\0") == 0) {
+			cout << "exit" << endl;
+			break; // null ¹®ÀÚ¿­ ¸¸³µÀ»¶§µµ loop Å»Ãâ
+		} else {
+			cout << "Incorrect password" << endl;
+		}
+	}
+	return 0;
+}
